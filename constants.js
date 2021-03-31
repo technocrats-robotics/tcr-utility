4ab4549d9bac45c6a238ef1a50a9c91c6001b672
Shivansh Goel <shivanshgoel21@gmail.com>
2021-03-31 12:39:47 +0530

    add constants for hosting

---
+++ constants.js
@@ -0,0 +1,16 @@
+// email credentials
+const SERVICE = 'gmail'
+const EMAIL = 'tcrvitcc@gmail.com'
+const PASSWORD = 'skgcymxjkreofwrs'
+
+
+// for feedback
+const EMAILTO = 'technocratsroboticsvit@gmail.com'
+
+// google photos
+const ALBUMID = 'Syn797zndgSyrKom7'
+
+// token
+const TOKEN = 'technocrats-robotics-batch-of-2021'
+
+module.exports = { SERVICE, EMAIL, PASSWORD, EMAILTO, ALBUMID, TOKEN }
